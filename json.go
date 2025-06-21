6f598a65b1097fadc44b66c6e31cb1c92b876584
yuvallinux <yuvddd05@gmail.com>
2025-06-21 16:06:37 +0300

    add err

--- json.go
+++ json.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Error: Responding with %d error: %v", code, msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 
@@ -18,5 +32,4 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(data)
-	return
 }
