7266be757d718cde697b6d9110c7adcd36f935f2
yuvallinux <yuvddd05@gmail.com>
2025-06-22 18:55:49 +0300

    create feed follow

--- init_db.sql
+++ init_db.sql
@@ -15,3 +15,12 @@ CREATE TABLE feeds (
     url TEST UNIQUE NOT NULL,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE feed_follows (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
+    UNIQUE(user_id, feed_id)
+);
